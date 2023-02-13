0d66d4609f43a1135dbf01ebf8e198b457a8a07b
+ <+>
2023-02-13 21:11:52 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,22 @@ require 'pry'
 
 class String
 
+  # Using the end_with method to check if a string ends with a given period.
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    self.split(/[.?!]+/).count
   end
 end
+
+# binding.pry
